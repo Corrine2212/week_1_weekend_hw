135eddfc54af4d20eba9c501b685943ef676ba7f
Corrine Sing <corrine.sing@gmail.com>
2023-03-24 16:13:54 +0000

    first commit

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,56 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, sold):
+    pet_shop["admin"]["pets_sold"] += sold
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"]) 
+
+def get_pets_by_breed(pet_shop, breed):
+    found_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            found_breed.append(pet)
+    return found_breed
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    else:
+        return None
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            pet_shop["pets"].remove(pet)
+     
+def add_pet_to_stock(pet_shop, new_pet):
+    if bool(new_pet):
+        pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customers):
+    return customers["cash"]
+
+def remove_customer_cash(customers, cash):
+    customers["cash"] -= cash
+
+def get_customer_pet_count(customers):
+    return len(customers["pets"])
+
+def add_pet_to_customer(customers, new_pet):
+    if bool(new_pet):
+        customers["pets"].append(new_pet)
+
