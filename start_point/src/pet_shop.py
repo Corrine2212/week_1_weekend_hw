fa26908c13feb3c8657d093187a133441fb06515
Corrine Sing <corrine.sing@gmail.com>
2023-03-27 10:29:27 +0100

    2nd commit

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -29,13 +29,15 @@ def find_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
             return pet
-    else:
-        return None
 
 def remove_pet_by_name(pet_shop, name):
     for pet in pet_shop["pets"]:
         if pet["name"] == name:
             pet_shop["pets"].remove(pet)
+
+# alt. method:
+# def remove_pet_by_name(pet_shop, name):
+#   pet_shop["pets"].remove(find_pet_by_name(pet_shop, name))
      
 def add_pet_to_stock(pet_shop, new_pet):
     if bool(new_pet):
@@ -54,3 +56,34 @@ def add_pet_to_customer(customers, new_pet):
     if bool(new_pet):
         customers["pets"].append(new_pet)
 
+
+# Below code is incorrect:
+# def customer_can_afford_pet(customers, new_pet):
+#     for customer in customers:
+#         if customer["cash"] >= new_pet["price"]:
+#             return True
+#         print("You have sufficient funds")
+#     else: 
+#             return False
+# print("you have insufficient funds")
+
+
+def customers_can_afford_pet(customers, new_pet):
+    if customers["cash"] >= new_pet["price"]:
+        return True
+    else:
+        return False
+
+# alt method:
+# def customers_can_afford_pet(customers, new_pet):
+#     return customers["cash"] >= new_pet["price"]
+    
+
+# RECAP THIS!!:   
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet != None and customers_can_afford_pet(customer, pet):
+        remove_pet_by_name(pet_shop, pet["name"])
+        add_pet_to_customer(customer, pet)
+        remove_customer_cash(customer, pet["price"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        increase_pets_sold(pet_shop, 1)
